2d225a2b9ed992de9a650233a1365512feef8e05
shajansheriff <shajansheriff@gmail.com>
2015-08-20 18:06:55 +0530

    Assets Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  
+  scope :krysta15 do
+    get '/' => 'welcome#index'
+  end
+
+
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
